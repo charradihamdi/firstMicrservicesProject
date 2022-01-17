bf8c6b259e3ca7730eb7c6942a566a39d5eebc80
hamdi <76883216+charradihamdi@users.noreply.github.com>
2022-01-17 23:36:13 +0100

    test api

---
+++ posts/index.server.js
@@ -0,0 +1,25 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser=require('body-parser')
+const {randomBytes}=require('crypto')
+const app = express()
+app.use(bodyParser.json())
+const posts={}
+app.get('/',(req,res)=>{
+    res.send(posts)
+})
+
+app.post('/',(req,res)=>{
+    const id = randomBytes(4).toString('hex')
+    const title = req.body
+    posts[id]= {
+        id,
+        title
+    }
+
+    res.status(201).send(posts[id])
+})
+
+app.listen(4000,()=>{
+    console.log('listenning in port 4000')
+})
