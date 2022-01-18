e1fb47878ccb0dfa0c88c054d2920f8f63b70302
hamdi <76883216+charradihamdi@users.noreply.github.com>
2022-01-18 15:08:47 +0100

    add query server

---
+++ query/query.server.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const cors =require('cors')
+const app = express()
+app.use(bodyParser.json())
+app.use(cors())
+
+const posts={}
+
+app.get('/post',(req,res)=>{
+
+})
+app.post('/events',(req,res)=>{
+const {type,data}= req.body 
+
+if(type==='postCreated'){
+const {id,title}= data
+posts[id]={id,title,comments:[]}
+}
+if(type==='commentCreated'){
+    const {type,data,postId}=data;
+    const post = posts[postId]
+    post.comments.push({id,content})
+}
+res.send({})
+})
+app.listen(4003,()=>{
+    console.log('listenning in port 4003')
+})
