4464a542911cd995037d8668e3c7286cda96b3ec
hamdi <76883216+charradihamdi@users.noreply.github.com>
2022-01-18 14:56:15 +0100

    related event-bus with posts and comment routes

--- posts/index.server.js
+++ posts/index.server.js
@@ -11,17 +11,25 @@ app.get('/',(req,res)=>{
     res.send(posts)
 })
 
-app.post('/',(req,res)=>{
+app.post('/',async (req,res)=>{
     const id = randomBytes(4).toString('hex')
     const title = req.body.title
     posts[id]= {
         id,
         title
     }
-    axios.post('localhost:4005/events')
+   await axios.post('localhost:4005/events',{
+        type:'postCreated',
+        data:{
+            id,title
+        }
+    })
     res.status(201).send(posts[id])
 })
-
+app.post('events',(req,res)=>{
+    console.log('received event',req.body.type)
+    res.send({})
+})
 app.listen(4000,()=>{
     console.log('listenning in port 4000')
 })
